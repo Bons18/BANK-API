9901f6cbe800cde5ca12ca39e68c9252dbdd419f
Bons18 <brayanrpabon@gmail.com>
2024-09-17 20:28:16 -0500

    v5

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const session = require('express-session');
 const connectDB = require('./config/db');
-const Usuario = require('./models/Usuario'); // Asegúrate de tener el modelo de Usuario
-const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const app = express();
@@ -12,15 +9,6 @@ connectDB();
 
 // Middleware para analizar JSON
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Para manejar datos del formulario
-
-// Configurar express-session
-app.use(session({
-    secret: 'tu_secreto_aqui', // Cambia esto por una clave secreta segura
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } // Cambia a true si usas HTTPS
-}));
 
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static('public'));
@@ -30,47 +18,9 @@ app.use('/api/clientes', require('./routes/clientes'));
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/cuentas', require('./routes/cuentasAhorro'));
 
-// Ruta para iniciar sesión
-app.post('/api/login', async (req, res) => {
-    const { nombreUsuario, password } = req.body;
-
-    try {
-        const usuario = await Usuario.findOne({ nombreUsuario });
-
-        if (!usuario) {
-            return res.status(400).json({ mensaje: 'Usuario no encontrado' });
-        }
-
-        const esValida = await bcrypt.compare(password, usuario.password);
-
-        if (!esValida) {
-            return res.status(400).json({ mensaje: 'Contraseña incorrecta' });
-        }
-
-        if (usuario.estado !== 'activo') {
-            return res.status(403).json({ mensaje: 'Usuario inactivo' });
-        }
-
-        req.session.usuario = usuario; // Guardar usuario en sesión
-
-        res.redirect('/');
-    } catch (error) {
-        res.status(500).json({ mensaje: 'Error interno del servidor' });
-    }
-});
-
 // Ruta principal
 app.get('/', (req, res) => {
-    if (req.session.usuario) {
-        res.sendFile(__dirname + '/public/index.html');
-    } else {
-        res.redirect('/login');
-    }
-});
-
-// Ruta para la página de inicio de sesión
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/public/login.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 // Manejo de errores 404
